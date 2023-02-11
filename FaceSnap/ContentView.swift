45a9b044b45f1d75bb3ab3d111732e367d285174
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-02-11 23:47:47 +0100

    Progression

--- FaceSnap/ContentView.swift
+++ FaceSnap/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject private var users = SharedPeople()
+    
+    @State private var showingImagePicker = false
+    @State private var askingForUsername = false
+    
+    @State private var inputImage: UIImage?
+    
+    @State private var photoID: String?
+    @State private var username: String?
+    
+    let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedFaces")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                List(users.personDetails.sorted()) { person in
+                    HStack {
+                        Image("\(person.photoID)")
+                        
+                        Text(person.name)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle("FaceSnap")
+            .toolbar {
+                Button {
+                    addUser()
+                } label: {
+                    Label("Add User", systemImage: "plus")
+                }
+            }
+            .onChange(of: inputImage) { _ in
+                if let jpegData = inputImage?.jpegData(compressionQuality: 0.8) {
+                    let uuid = UUID().uuidString
+                    
+                    photoID = uuid
+                    
+                    try? jpegData.write(to: savePath.appendingPathComponent(uuid), options: [.atomic, .completeFileProtection])
+                    
+                    askingForUsername = true
+                }
+            }
+            .sheet(isPresented: $showingImagePicker) {
+                ImagePicker(image: $inputImage)
+            }
+            .sheet(isPresented: $askingForUsername) {
+                PersonNameView()
+            }
         }
-        .padding()
+    }
+    
+    func addUser() {
+        users.personDetails.append(Person.example)
     }
 }
 
---
+++ FaceSnap/Person.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  FaceSnap
+//
+//  Created by Edwin Przeźwiecki Jr. on 11/02/2023.
+//
+
+import Foundation
+
+class SharedPeople: ObservableObject {
+    @Published var personDetails = [Person]()
+}
+
+struct Person: Codable, Equatable, Identifiable, Comparable {
+    var id = UUID()
+    var photoID: UUID
+    var name: String
+    
+    static func <(lhs: Person, rhs: Person) -> Bool {
+        lhs.name < rhs.name
+    }
+    
+    static var example = Person(id: UUID(), photoID: UUID(), name: "Harry Potter")
+}
