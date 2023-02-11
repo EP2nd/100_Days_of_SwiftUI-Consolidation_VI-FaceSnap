e6ff25511447b30b210006c15ceccbaa98c49bb2
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-02-11 18:12:17 +0100

    Initial Commit

---
+++ FaceSnap/FaceSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceSnapApp.swift
+//  FaceSnap
+//
+//  Created by Edwin Przeźwiecki Jr. on 11/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FaceSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
